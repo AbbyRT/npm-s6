d2f24a0b2402a7261c0b5ca273acca0213daa63e
Abigail Romero <romerot.abigail@gmail.com>
2021-10-01 20:28:15 -0500

    primer ejercicio con npm, sweetalert y parcel

---
+++ src/assets/js/main.js
@@ -0,0 +1,8 @@
+import Swal from 'sweetalert2';
+/* se puede importar y exportar codigo de dif archivos */
+Swal.fire({
+    title: 'Error!',
+    text: 'Do you want to continue',
+    icon: 'error',
+    confirmButtonText: 'Cool'
+  })
